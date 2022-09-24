757c9c604e03b10da17b4595e13bd8afc03c14be
Beth <bklassen30@gmail.com>
2022-09-24 18:37:54 -0500

    Add Q&A

--- src/routes/questions.svelte
+++ src/routes/questions.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <svelte:head>
-	<title>Accommodations| Beth & Josh</title>
+	<title>Q&A | Beth & Josh</title>
 	<meta name="description" content="Accommodations" />
 	<meta property="og:image" content="/welcometotheclub.gif" />
 </svelte:head>
@@ -16,29 +16,36 @@
 
 <div class="mainstuff">
 	<div style="text-align: center; margin-top: 1%;">
-		<img src="/images/gothicpointing.jpg" alt="Beth and Josh pointing at you" />
+		<img src="/images/bridgeface.jpg" alt="Beth and Josh by a bridge" />
 	</div>
 	<br />
 	<br />
 	<hr class="smalldivider" />
 	<h2>Questions</h2>
 	<hr class="smalldivider" />
+	<h3>When do I need to RSVP?</h3>
+	<p>Please RSVP before May 30, 2023.</p>
 	<h3>Can I bring a date?</h3>
-	<p>Yes! Please have your date fill out an RSVP.</p>
+	<p>Yes! Please indicate on your RSVP.</p>
 	<h3>Are kids welcome?</h3>
 	<p>Absolutely! Please indicate the number of kids and any highchairs needed on your RSVP.</p>
-	<h3>Can I bring a date?</h3>
-	<p>Yes! Please have your date fill out an RSVP.</p>
-	<h3>Are kids welcome?</h3>
-	<p>Absolutely! Please indicate the number of kids and any highchairs needed on your RSVP.</p>
-	<h3>Can I bring a date?</h3>
-	<p>Yes! Please have your date fill out an RSVP.</p>
-	<h3>Are kids welcome?</h3>
-	<p>Absolutely! Please indicate the number of kids and any highchairs needed on your RSVP.</p>
-	<h3>Can I bring a date?</h3>
-	<p>Yes! Please have your date fill out an RSVP.</p>
-	<h3>Are kids welcome?</h3>
-	<p>Absolutely! Please indicate the number of kids and any highchairs needed on your RSVP.</p>
+	<h3>What should I wear to the wedding?</h3>
+	<p>This is a once in a lifetime event for us, so we'd love for you to dress up!</p>
+	<h3>What colors are the wedding party wearing?</h3>
+	<p>The bridal party will be wearing burnt orange attire.</p>
+	<h3>Is there parking?</h3>
+	<p>
+		We encourage you to take a cab or walk if you are staying nearby, otherwise there is parking at
+		the <a
+			href="https://www.google.com/maps/place/Brewery+Parking+Structure/@43.0457378,-87.9246908,18z/data=!4m12!1m6!3m5!1s0x8805197097bbc625:0x520f6810dd74598d!2sBest+Place+at+the+Historic+Pabst+Brewery!8m2!3d43.0455111!4d-87.9233972!3m4!1s0x88051995855600b7:0xda5d12a2f242fc59!8m2!3d43.0463128!4d-87.9232916"
+			>Brewery District Garage.</a
+		>
+	</p>
+	<h3>Who can I reach out to with questions?</h3>
+	<p>Please reach out to Beth and Josh directly.</p>
+	<br />
+	<br />
+	<br />
 	<br />
 </div>
 
