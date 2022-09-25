6f2c405ce119b33210d8ac2e79a547a54041cfe9
Beth Klassen <62486499+bklassy@users.noreply.github.com>
2022-09-24 19:24:20 -0500

    Contentupdate (#7)
    * Add Q&A
    
    * updating our story

--- src/routes/ourstory.svelte
+++ src/routes/ourstory.svelte
@@ -1,7 +1,6 @@
 <script>
 	import Navbar from '$lib/navbar.svelte';
 	import Header from '$lib/header.svelte';
-	import Weddinginfo from '$lib/weddinginfo.svelte';
 </script>
 
 <svelte:head>
@@ -20,14 +19,37 @@
 	</div>
 	<h2>Our Story</h2>
 	<hr class="smalldivider" />
-	<p>
-		Beth and Josh Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-		incididunt ut labore et dolore magna aliqua. Sollicitudin nibh sit amet commodo nulla facilisi.
-		Ultrices tincidunt arcu non sodales neque sodales ut. Volutpat lacus laoreet non curabitur
-		gravida. Libero id faucibus nisl tincidunt eget nullam non. Id consectetur purus ut faucibus.
-		Quis viverra nibh cras pulvinar mattis. Est lorem ipsum dolor sit amet consectetur adipiscing
-		elit. Odio eu feugiat pretium nibh. Et tortor consequat id porta nibh.
-	</p>
+	<div class="thestory">
+		<p>
+			Beth and Josh's story starts when the two found themselves at several of the same college
+			outings while attending UW-Madison. Beth, working as a student caterer, had befriended Josh's
+			catering roommates and had no idea that through her new friends, she would meet the boy of her
+			dreams. Josh, a shy computer engineering student with a less shy sense of humor, charmed Beth
+			with his witty and dry remarks. He was truly smitten by her sense of humor, and the two began
+			dating their senior year of college.
+		</p>
+		<p>
+			After graduation, they embarked on a long-distance journey, as Josh had accepted a job in
+			Austin, while Beth started working in Madison. For almost two years they would travel back and
+			forth between cities on weekends, spending many weeknights on Google Hangouts and tallying
+			their text count as it hit the ten thousands.
+		</p>
+		<p>
+			Having had their fair share of missed connections and lonely nights, they decided to be
+			together in Madison. Josh was lucky to find a remote job (before it was cool!) so he could
+			move to Madison in 2018. The two lived happily ever after...
+		</p>
+		<p>
+			But wait! After a year together in Madison, it was time for them to experience a different
+			city. After much debate, Beth and Josh realized Austin was the place where they fell in love,
+			and wanted to experience more. They moved to Austin in August 2019 and have lived there ever
+			since, enjoying the breakfast tacos, live music, and breweries.
+		</p>
+		<p>
+			In 2022, after 6 years together, Josh proposed in the courtyard near their apartment. Beth, of
+			course, said yes, and the couple is excited to celebrate their love and commitment with you.
+		</p>
+	</div>
 	<br />
 	<br />
 </div>
@@ -40,6 +62,14 @@
 		justify-content: center;
 		flex-direction: column;
 		font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+	}
+
+	.thestory {
+		width: 80%;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
 	}
 
 	@media screen and (max-width: 900px) {
@@ -61,11 +91,11 @@
 		}
 	}
 
-		h2,
-		p,
-		.smalldivider {
-			animation: fadeIn ease 1.5s;
-		}
+	h2,
+	p,
+	.smalldivider {
+		animation: fadeIn ease 1.5s;
+	}
 	h2 {
 		width: 40%;
 		text-align: center;
