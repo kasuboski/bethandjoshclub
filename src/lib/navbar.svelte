87308c0d67ee239715727b565f6da2ceee54e6fc
Beth <bklassen30@gmail.com>
2022-10-18 16:07:49 -0500

    lower case rsvp

--- src/lib/navbar.svelte
+++ src/lib/navbar.svelte
@@ -1,15 +1,31 @@
 <script>
-        import { page } from '$app/stores';
-    </script>
+	import { page } from '$app/stores';
+</script>
 
+<nav class="navbar">
+	<ul>
+		<a href="/home" class:active={$page.url.pathname.includes('/home')}>Home</a>
+		<a href="/rsvp" class:active={$page.url.pathname.includes('/rsvp')}>RSVP</a>
+		<a href="/ourstory" class:active={$page.url.pathname.includes('/ourstory')}>Our Story</a>
+		<a href="/schedule" class:active={$page.url.pathname.includes('/schedule')}>Schedule</a>
+		<a href="/accommodations" class:active={$page.url.pathname.includes('/accommodations')}
+			>Accommodations</a
+		>
+		<a href="/questions" class:active={$page.url.pathname.includes('/questions')}>Q&A</a>
+		<a href="/photos" class:active={$page.url.pathname.includes('/photos')}>Photos</a>
+		<a href="/registry" class:active={$page.url.pathname.includes('/registry')}>Registry</a>
+	</ul>
+</nav>
 
+<hr />
+
+<!-- need to add responsive menu, max width of 900px will need to be hamburger menu -->
 <style>
-
-.navbar {
-		display:flex;
+	.navbar {
+		display: flex;
 		flex: 1;
 		justify-content: center;
-		width:100%;
+		width: 100%;
 		font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
 	}
 	.active {
@@ -17,17 +33,16 @@
 	}
 
 	ul {
-		display:flex;
-		justify-content: space-between; 
-		width:60%;
+		display: flex;
+		justify-content: space-between;
+		width: 60%;
 		padding: 0;
 	}
 
 	a {
 		text-decoration: none;
 		color: black;
-		margin: 0em .25em;
-		
+		margin: 0em 0.25em;
 	}
 
 	hr {
@@ -37,34 +52,14 @@
 		margin-bottom: 3%;
 	}
 
-
-@media screen and (max-width: 900px) {
-
-	ul {
-		flex-direction: column;
-		text-align: center;
+	@media screen and (max-width: 900px) {
+		ul {
+			flex-direction: column;
+			text-align: center;
+		}
+		a {
+			font-size: 1em;
+			margin-top: 0.25em;
+		}
 	}
-	a {
-		font-size: 1em;
-		margin-top: .25em;
-	}
-}
-
 </style>
-
-<nav class="navbar">
-	<ul>
-		<a href="/home" class:active="{$page.url.pathname.includes("/home")}">Home</a>
-		<a href="/RSVP" class:active="{$page.url.pathname.includes("/RSVP")}">RSVP</a>
-		<a href="/ourstory" class:active="{$page.url.pathname.includes("/ourstory")}">Our Story</a>
-		<a href="/schedule" class:active="{$page.url.pathname.includes("/schedule")}">Schedule</a>
-		<a href="/accommodations" class:active="{$page.url.pathname.includes("/accommodations")}">Accommodations</a>
-		<a href="/questions" class:active="{$page.url.pathname.includes("/questions")}">Q&A</a>
-		<a href="/photos" class:active="{$page.url.pathname.includes("/photos")}">Photos</a>
-		<a href="/registry" class:active="{$page.url.pathname.includes("/registry")}">Registry</a>
-	</ul>
-</nav>
-
-<hr/>
-
-<!-- need to add responsive menu, max width of 900px will need to be hamburger menu -->
